ab826d6df6bebf592d12ae0a69181ce83d54ac27
Dienisson Chinelatto <dienissonchinelatto@gmail.com>
2020-04-26 10:27:08 -0300

    Add Migrations.

---
+++ Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    public class LojaContext : DbContext
+    {
+        DbSet<Produto> Produtos { get; set; }
+
+        public LojaContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+        }
+    }
+}
